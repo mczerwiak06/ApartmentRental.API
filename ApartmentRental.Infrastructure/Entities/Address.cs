03acdc38a651306527eebbd9673242d353ece23c
Michał Czerwiak <michal.czerwiak@gmail.com>
2022-05-04 17:51:26 +0200

    App before migration

---
+++ ApartmentRental.Infrastructure/Entities/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace ApartmentRental.Infrastructure.Entities;
+
+public class Address : BaseEntity
+{
+    public string Street { get; set; }
+    public string? AppartmentNumber { get; set; }
+    public string? BuildingNumber { get; set; }
+    public string City { get; set; }
+    public string PostCode { get; set; }
+    public string Country { get; set; }
+}
