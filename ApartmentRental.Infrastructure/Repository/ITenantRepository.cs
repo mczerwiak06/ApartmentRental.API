a160d9c9438477f35dc988e27e41a2e0c8ed826c
Michał Czerwiak <michal.czerwiak@gmail.com>
2022-05-05 12:18:49 +0200

    Lab3 Zaimplementowano interfejs IRepository<T>

---
+++ ApartmentRental.Infrastructure/Repository/ITenantRepository.cs
@@ -0,0 +1,8 @@
+﻿using ApartmentRental.Infrastructure.Entities;
+
+namespace ApartmentRental.Infrastructure.Repository;
+
+public interface ITenantRepository : IRepository<Tenant>
+{
+    
+}
---
+++ ApartmentRental.Infrastructure/Repository/ImageRepository.cs
@@ -0,0 +1,31 @@
+﻿using ApartmentRental.Infrastructure.Entities;
+
+namespace ApartmentRental.Infrastructure.Repository;
+
+public class ImageRepository : IImageRepository
+{
+    public Task Add(Image entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Image>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Image> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(Image entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
